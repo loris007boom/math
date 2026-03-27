d8d62dd12c99dbf706b9b27922d9a74fcac088d9
Loris.FestoniPerillo <loris.festoniperillo@swisscom.com>
2026-03-27 15:38:43 +0100

    Implementing encompassing logic for rectangle and circle

--- geometry.ts
+++ geometry.ts
@@ -14,6 +14,14 @@ export class Point2D {
       Math.abs(this.x - other.x) ** 2 + Math.abs(this.y - other.y) ** 2,
     );
   }
+
+  isBetweenX(p: Point2D, q: Point2D): boolean {
+    return (p.x < this.x && this.x < q.x) || (q.x < this.x && this.x < p.x);
+  }
+
+  isBetweenY(p: Point2D, q: Point2D): boolean {
+    return (p.y < this.y && this.y < q.y) || (q.y < this.y && this.y < p.y);
+  }
 }
 
 export class Circle implements Shape {
@@ -33,6 +41,38 @@ export class Circle implements Shape {
   diameter(): number {
     return 2 * this.radius;
   }
+
+  middle(): Point2D {
+    return this.center;
+  }
+
+  north(): Point2D {
+    return new Point2D(this.center.x, this.center.y + this.radius);
+  }
+
+  east(): Point2D {
+    return new Point2D(this.center.x + this.radius, this.center.y);
+  }
+
+  south(): Point2D {
+    return new Point2D(this.center.x, this.center.y - this.radius);
+  }
+
+  west(): Point2D {
+    return new Point2D(this.center.x - this.radius, this.center.y);
+  }
+
+  encompasses(other: Shape): boolean {
+    if (other instanceof Rectangle) {
+      return [
+        other.a(),
+        other.b(),
+        other.c(),
+        other.d(),
+      ].every((point) => this.center.distanceTo(point) < this.radius);
+    }
+    return false;
+  }
 }
 
 export class Rectangle implements Shape {
@@ -51,6 +91,38 @@ export class Rectangle implements Shape {
 
   diagonal(): number {
     return this.bottomLeft.distanceTo(this.topRight);
+  }
+
+  a(): Point2D {
+    return this.bottomLeft;
+  }
+
+  b(): Point2D {
+    return new Point2D(this.topRight.x, this.bottomLeft.y);
+  }
+
+  c(): Point2D {
+    return this.topRight;
+  }
+
+  d(): Point2D {
+    return new Point2D(this.bottomLeft.x, this.topRight.y);
+  }
+
+  encompasses(other: Shape): boolean {
+    if (other instanceof Circle) {
+      return [
+        other.middle(),
+        other.north(),
+        other.east(),
+        other.south(),
+        other.west(),
+      ].every((point: Point2D) =>
+        point.isBetweenX(this.a(), this.b()) &&
+        point.isBetweenY(this.a(), this.d())
+      );
+    }
+    return false;
   }
 
   private width(): number {
--- geometry_test.ts
+++ geometry_test.ts
@@ -1,5 +1,5 @@
-import { assertAlmostEquals } from "@std/assert";
-import { Circle, Point2D } from "./geometry.ts";
+import { assertAlmostEquals, assertEquals } from "@std/assert";
+import { Circle, Point2D, Rectangle } from "./geometry.ts";
 
 Deno.test("circumference of a circle with radius 5 is roughtly 31.416", () => {
   // Given
@@ -10,4 +10,65 @@ Deno.test("circumference of a circle with radius 5 is roughtly 31.416", () => {
 
   // Then
   assertAlmostEquals(actual, 31.416, 0.01);
+});
+
+Deno.test("north east south west points are computed correctly", () => {
+  const circle = new Circle(new Point2D(5, 5), 5);
+
+  assertEquals(circle.north(), new Point2D(5, 10));
+  assertEquals(circle.east(), new Point2D(10, 5));
+  assertEquals(circle.south(), new Point2D(5, 0));
+  assertEquals(circle.west(), new Point2D(0, 5));
+});
+
+Deno.test("point is between west and east on x-axis", () => {
+  const m = new Point2D(5, 5);
+  const w = new Point2D(0, 5);
+  const e = new Point2D(10, 5);
+
+  assertEquals(m.isBetweenX(w, e), true);
+});
+
+Deno.test("point is between south and north on y-axis", () => {
+  const m = new Point2D(5, 5);
+  const s = new Point2D(5, 0);
+  const n = new Point2D(5, 10);
+
+  assertEquals(m.isBetweenY(s, n), true);
+});
+
+Deno.test("point is not between west and east on y-axis", () => {
+  const m = new Point2D(5, 5);
+  const w = new Point2D(0, 5);
+  const e = new Point2D(10, 5);
+
+  assertEquals(m.isBetweenY(w, e), false);
+});
+
+Deno.test("rectangle encompasses circle", () => {
+  const rectangle = new Rectangle(new Point2D(0, 0), new Point2D(10, 10));
+  const circle = new Circle(new Point2D(5, 5), 4);
+
+  assertEquals(rectangle.encompasses(circle), true);
+});
+
+Deno.test("rectangle does not encompass circle when radius is too large", () => {
+  const rectangle = new Rectangle(new Point2D(0, 0), new Point2D(10, 10));
+  const circle = new Circle(new Point2D(5, 5), 5);
+
+  assertEquals(rectangle.encompasses(circle), false);
+});
+
+Deno.test("circle encompasses rectangle", () => {
+  const circle = new Circle(new Point2D(5, 5), 8);
+  const rectangle = new Rectangle(new Point2D(2, 2), new Point2D(8, 8));
+
+  assertEquals(circle.encompasses(rectangle), true);
+});
+
+Deno.test("circle does not encompass rectangle", () => {
+  const circle = new Circle(new Point2D(5, 5), 3);
+  const rectangle = new Rectangle(new Point2D(2, 2), new Point2D(8, 8));
+
+  assertEquals(circle.encompasses(rectangle), false);
 });
